b6435e9ac6f400bb9a0d14f53fdd9a8b4c4f74dd
ng-tin08 <dngoctien04@gmail.com>
2024-10-20 19:53:22 +0700

    Initial commit in my-app

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,10 @@
+const WelcomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to my Website</h1>
+            <p>You have successfully registered.</p>
+        </div>
+    );
+}
+
+export default WelcomePage;
